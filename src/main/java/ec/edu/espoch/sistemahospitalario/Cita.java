54033368d066ee1ffc15d391ed05945f169fd1cb
jenni-armijos08 <jenniarmijos08@gmail.com>
2026-03-30 02:13:47 -0500

    first commit

---
+++ src/main/java/ec/edu/espoch/sistemahospitalario/Cita.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espoch.sistemahospitalario;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class Cita {
+    
+}
---
+++ src/main/java/ec/edu/espoch/sistemahospitalario/SistemaHospitalario.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.sistemahospitalario;
+
+/**
+ *
+ * @author PERSONAL
+ */
+public class SistemaHospitalario {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
